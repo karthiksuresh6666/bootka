6e6b874311a6b096fa79e5086c3e849845fcf765
Karthik Suresh <karthik.suresh@siemens.com>
2021-05-07 13:24:34 +0530

    first

---
+++ src/main/java/com/rebel/bootka/BootkaApplication.java
@@ -0,0 +1,13 @@
+package com.rebel.bootka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebel/bootka/BootkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.bootka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
